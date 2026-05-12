8c2920b9f6253f5d59811e642faf4dee3ea04321
Trepidkhabirov <pushok12345678910@gmail.com>
2026-05-12 22:24:54 +0500

    Vue начало

---
+++ vue translog/trans_log/src/auth/authorization.vue
@@ -0,0 +1,26 @@
+<script setup>
+import logo from '../assets/images/logo.png'
+import './Authorization.css'
+</script>
+
+<template>
+  <div class="auth">
+    <div class="label">
+        <form id="background">
+            <img :src="logo" class="logo">
+            
+            <h1 id="translog">ТРАНСЛОГ</h1>
+            <p>УЧЕТ ЗАЯВОК НА ГРУЗПЕРЕВОЗКИ</p>
+            
+            <p>Логин</p>
+            <input placeholder="Введите логин" type="text" required>
+            
+            <p>Пароль</p>
+            <input placeholder="********" type="password">
+            
+            <button type="submit">Войти</button>
+            <a id="noaccount" href="#">Нет аккаунта?</a>
+        </form>
+    </div>
+  </div>
+</template>
